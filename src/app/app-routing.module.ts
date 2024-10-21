c31a96fabd21d3918a197aa8511a41127a8ef23a
alejo190404 <alejo190404@gmail.com>
2024-10-21 10:18:43 -0500

    Creación inicial del componente footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
-  {path: "header", component: HeaderComponent},
+  {path: "", component: FooterComponent},
 ];
 
 @NgModule({
