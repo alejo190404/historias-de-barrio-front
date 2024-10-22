3d0fdf8c97bebff1ef376a0a2ce82d02646af933
alejo190404 <alejo190404@gmail.com>
2024-10-21 21:59:13 -0500

    Landing finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
+  {path: "", component: LandingPageComponent},
   {path: "header", component: HeaderComponent},
 ];
 
